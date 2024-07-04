ba69c613c6e6daa5e32c93dd81dcb21b7e502731
Najm <nazmuddin@gamerpe.in>
2024-07-05 01:26:35 +0530

    initial commit

---
+++ src/components/Shortner.jsx
@@ -0,0 +1,79 @@
+import React,{useRef,useState,useEffect} from 'react'
+import '../style/Shortner.css'
+import Link_shortner from '../assets/link_shortner.png'
+
+const Shortner = () => {
+    const url=useRef()
+    const [shortUrl,setShortUrl]=useState(null)
+
+    useEffect(()=>{
+        
+    },[shortUrl])
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault()
+        const originalUrl=url.current.value
+
+        try {
+            const response=await fetch('https://url-shortner-server-xi.vercel.app/api/urlshortner',{
+                method:'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({ originalUrl }),
+            })
+            const data = await response.json();
+            setShortUrl(data.data.shortUrl)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+
+    const handleCopy=async()=>{
+        await navigator.clipboard.writeText(shortUrl)
+    }
+
+
+
+  return (
+    <>
+      <header className="hero">
+        <div className="hero-inner">
+
+          <div className="hero-text">
+            <h2>Hi There ! Welcome to Url Shortner</h2>
+            <br/>
+            <br/>
+            <p>Step -1 Copy your Long URL</p>
+            <p>Step -2 Paste your Long URL into input field</p>
+            <p>Step -3 Press Submit and then use</p>
+            <form className="hero-form" onSubmit={handleSubmit}>
+              <div className="hero-form-input">
+                <input className="hero-email-input" type="url" ref={url} placeholder="eg:- https://www.example.com/xyz...." required/>
+                <input className="hero-form-submit" type="submit" value="Submit"/>
+                
+              </div>
+            </form>
+
+            <form className="hero-form" onSubmit={handleCopy}>
+              <div className="hero-form-input">
+                <input className="hero-email-input" type="text" value={shortUrl} required/>
+                <input className="hero-form-copy" type="submit" value="Copy"/>
+                
+              </div>
+            </form>
+          </div>
+
+          <div className="hero-image">
+           
+            <img src={Link_shortner} alt='link image'/>
+          </div>
+
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Shortner
